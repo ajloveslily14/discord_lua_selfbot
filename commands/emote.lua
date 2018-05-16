7c98987139c3356fa5e2a33cc3aa6ab257d138d9
ajloveslily14 <ajloveslily@gmail.com>
2018-05-15 20:28:37 -0700

    Added emote command

---
+++ commands/emote.lua
@@ -0,0 +1,21 @@
+require("util")
+
+addCommand("emote",true,function(m,args)
+
+	local emote = emoteFromString(args[1])
+	emote = client:getEmoji(emote)
+	if emote then
+		local rep = newEmbed()
+		local desc = string.format("%s :%s:\nId: %s\nGuild Name: %s",emote.mentionString,emote.name,emote.id,emote.guild.name)
+		rep:setDescription(desc)
+		rep:addImage()
+		rep:setImage(emote.url)
+		rep:setColor(emote.guild:getMember(client.user):getColor())
+		m:setContent(nil)
+		m:setEmbed(rep)
+	else
+		m:setContent("Couldn't find that emote?")
+		timer.sleep(3000)
+		m:delete()
+	end
+end)

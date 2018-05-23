97f73e9f8ae6d0f5b7293bdbbaafb3efc53ac53f
ajloveslily14 <ajloveslily@gmail.com>
2018-05-22 23:19:39 -0700

    whoops

--- commands/avatar.lua
+++ commands/avatar.lua
@@ -4,9 +4,9 @@ addCommand("avatar",function(m,args) -- Command to retrieve the avatar of a user
 	local url = userFromMention(args[1]) -- Get user object from mention
 	if url then
 		local rep = newEmbed()
-		rep:setTitle(m.author.tag)
+		rep:setTitle(url.tag)
 		rep:addImage()
-		rep:setImage(m.author:getAvatarURL().."?size=1024")
+		rep:setImage(url:getAvatarURL().."?size=1024")
 		m:delete()
 		m:reply({embed = rep})
 	else

6ed8c8689e8b3920aa3dd53cdf05d620db335c60
ajloveslily14 <ajloveslily@gmail.com>
2018-05-22 22:19:46 -0700

    make this an embed

--- commands/avatar.lua
+++ commands/avatar.lua
@@ -3,7 +3,12 @@ addCommand("avatar",function(m,args) -- Command to retrieve the avatar of a user
 
 	local url = userFromMention(args[1]) -- Get user object from mention
 	if url then
-		m:setContent(url:getAvatarURL().."?size=1024")
+		local rep = newEmbed()
+		rep:setTitle(m.author.fullname)
+		rep:addImage()
+		rep:setImage(m.author:getAvatarURL().."?size=1024")
+		m:delete()
+		m:reply({embed = rep})
 	else
 		m:setContent("Invalid user")
 		timer.sleep(3000)

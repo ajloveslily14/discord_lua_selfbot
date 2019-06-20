2813f9d74e6b0983fe6d9bd4e0dccb868fa06e10
ajloveslily14 <ajloveslily@gmail.com>
2019-06-19 19:32:49 -0700

    fix blocks command

--- commands/blocks.lua
+++ commands/blocks.lua
@@ -3,7 +3,7 @@ require("util")
 addCommand("blocks",function(m,args)
 	if args[1] == "" then return end
 	
-	local rep = table.concat(args,","_)
+	local rep = table.concat(args,",")
 
 	m:setContent(string.gsub(rep,"%a",":regional_indicator_%1:\226\128\139"):lower())
 end)

ba6c8295490fe9ab87ebc86100a1ee9ce2fe7afa
ajloveslily14 <ajloveslily@gmail.com>
2019-06-30 22:58:21 -0700

    Add a random color for avatars

--- commands/avatar.lua
+++ commands/avatar.lua
@@ -7,6 +7,7 @@ addCommand("avatar",function(m,args) -- Command to retrieve the avatar of a user
 		rep:setTitle(url.tag)
 		rep:addImage()
 		rep:setImage(url:getAvatarURL().."?size=1024")
+		rep:setColor(math.random(0xFFFFFF))
 		m:delete()
 		m:reply({embed = rep})
 	else

63a5115fe5d3e8f57ff68b7b69ad8d0e4909c51a
ajloveslily14 <ajloveslily@gmail.com>
2019-09-02 22:02:18 -0700

    Remove this trash

--- commands/omega.lua
+++
@@ -1,9 +0,0 @@
-require("util")
-
-addCommand("lul",function(m,args)
-	if args[1] == "" then return end
-
-	local rep = table.concat(args,",")
-
-	m:setContent(string.gsub(rep,"[oO]","<:omegalul:450179262876614657>"))
-end)

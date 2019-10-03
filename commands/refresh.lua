b34c195bc85a8d9192838151c77c2dcd12e3cbe3
ajloveslily14 <ajloveslily@gmail.com>
2019-10-03 12:49:01 -0700

    Rewrite the refresh to make it neater
    Side note all commits from now on should have unix return lines

--- commands/refresh.lua
+++ commands/refresh.lua
@@ -1,47 +1,55 @@
 require("util")
 
-function refreshFile(m,args) -- Command to refresh other commands.
-	local reload
-	if args[1] ~= "" then
-		local todo = args[1]..".lua"
-		local f = io.popen("ls commands","r") -- Iterate through all files in commands/
-		for v in f:lines() do
-			if v == todo then
-				reload = v 
-			end
-		end
-		f:close()
-		if reload then -- If the command we are trying to refresh is valid, reload the file.
-			local f,e = loadfile("commands/"..reload) -- Check for syntax errors
-				if not f then
-					m:setContent("Error loading "..reload..": ```"..e.."```")
-					timer.sleep(5000)
-					m:delete()
-				else
-					setfenv(f,_G)
-					local func,err = pcall(f) -- Check for runtime errors.
-					if not func then
-						m:setContent("Error loading "..reload..": ```"..err.."```")
-						timer.sleep(5000)
-						m:delete()
-					else
-						m:setContent("Reloaded "..reload)
-						timer.sleep(3000)
-						m:delete()
-					end
-				end
-		else
-			m:setContent("Couldn't find "..todo)
-			timer.sleep(5000)
-			m:delete()
-		end
+
+local function isCommand(str) -- Check to make sure the command exists
+	local name = ""
+	if str:sub(-4) == ".lua" then -- If we added a lua extension keep it, else just add one.
+		name = str
 	else
-		m:setContent("I need args!")
-		timer.sleep(3000)
-		m:delete()
+		name = str..".lua"
 	end
+	local f = io.popen("ls commands","r") -- Get a list of files in commands/
+	for line in f:lines() do
+		if line == name then
+			return name
+		end
+	end
+	return false
 end
 
+
+function refreshFile(m,args)
+	if args[1] == "" then
+		m:setContent("I need a file!")
+		timer.sleep(3000)
+		m:delete()
+		return
+	end
+	local file = isCommand(args[1]) -- Check if the input is actually a file.
+	if not file then
+		m:setContent("Can't find that file")
+		timer.sleep(5000)
+		m:delete()
+		return
+	end
+
+	local func,err = loadfile("commands/"..file) -- Load the file and check for syntax errors
+	if not func then
+		m:setContent("Syntax error while loading "..file..": ```"..err.."```")
+		timer.sleep(5000)
+		m:delete()
+		return
+	end
+	setfenv(func,_G)
+	local ok,err = pcall(func) -- Run the code and check for runtime errors
+	if not ok then
+		m:setContent("Runtime error while loading "..file..": ```"..err.."```")
+		timer.sleep(5000)
+		m:delete()
+		return
+	end
+	m:setContent("Reloaded "..file)
+end
 
 addCommand("reload",refreshFile)
 addCommand("refresh",refreshFile)
